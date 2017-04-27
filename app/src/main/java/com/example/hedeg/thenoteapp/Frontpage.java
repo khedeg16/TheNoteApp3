1e18d7c53802b329133527ff35c08cb99a8bb12c
Thomas <thomaslpedersen1995@gmail.com>
2017-04-27 21:50:44 +0200

    removed comment about ksaper being a dick

--- app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
+++ app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
@@ -1,5 +1,6 @@
 package com.example.hedeg.thenoteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,12 @@ public class Frontpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
     }
+}
+
+
+//Called when 'see notes'-button is clicked//
+public void launchMain(){
+    Intent intentLaunchMain = new Intent(this, MainActivity.class);
+    startActivity(intentLaunchMain);
+}
 }
