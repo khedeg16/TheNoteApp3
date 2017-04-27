e097a79531f25298e4a218eae7dbcd42717a44fc
Thomas <thomaslpedersen1995@gmail.com>
2017-04-27 22:06:13 +0200

    See notes button work on frontpage

--- app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
+++ app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
@@ -3,6 +3,7 @@ package com.example.hedeg.thenoteapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Frontpage extends AppCompatActivity {
 
@@ -11,12 +12,12 @@ public class Frontpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
     }
-}
+
 
 
 //Called when 'see notes'-button is clicked//
-public void launchMain(){
-    Intent intentLaunchMain = new Intent(this, MainActivity.class);
-    startActivity(intentLaunchMain);
+public void launchMain(View view){
+    Intent intentLaunchMain = new Intent(Frontpage.this, MainActivity.class);
+    Frontpage.this.startActivity(intentLaunchMain);
 }
 }
