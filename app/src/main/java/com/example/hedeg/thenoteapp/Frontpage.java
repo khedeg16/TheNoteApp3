0acdffd9e0ed50a8027412918fae4e646b7174db
Thomas <thomaslpedersen1995@gmail.com>
2017-04-27 22:27:22 +0200

    The frontpage work

--- app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
+++ app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Frontpage extends AppCompatActivity {
+
+    Button launch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage);
+        launch = (Button) findViewById(R.id.postNewNote);
+        launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchNewNote(v);
+            }
+        });
     }
 
 
@@ -19,5 +29,10 @@ public class Frontpage extends AppCompatActivity {
 public void launchMain(View view){
     Intent intentLaunchMain = new Intent(Frontpage.this, MainActivity.class);
     Frontpage.this.startActivity(intentLaunchMain);
+}
+
+public void launchNewNote(View view){
+    Intent intentLaunchNewNote = new Intent(Frontpage.this, EditorActivity.class);
+    Frontpage.this.startActivity(intentLaunchNewNote);
 }
 }
