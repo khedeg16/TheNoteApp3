7a239920f02c354049ab82e7b60b8b7cdef26b89
Thomas <thomaslpedersen1995@gmail.com>
2017-04-27 15:22:18 +0200

    Layout done

---
+++ app/src/main/java/com/example/hedeg/thenoteapp/Frontpage.java
@@ -0,0 +1,13 @@
+package com.example.hedeg.thenoteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Frontpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frontpage);
+    }
+}
